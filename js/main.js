99c9dee8342ac7a1bb6b071adb531d5dbc24a779
Tobin Bradley <tobin.bradley@gmail.com>
2013-01-08 19:05:38 -0500

    bug fix, data changes, latest bootstrap css

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
     // Load JSON metric configuration
     $.ajax({
-        url: "js/metrics.json?V=17",
+        url: "js/metrics.json?V=18",
         dataType: "json",
         async: false,
         success: function(data){
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
     // Grab NPA JSON
     $.ajax({
-        url: "js/npa.json?V=17",
+        url: "js/npa.json?V=18",
         dataType: "json",
         type: "GET",
         async: false,
