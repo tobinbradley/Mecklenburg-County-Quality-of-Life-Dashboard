89fb35d541758cf3f9b9d455a09e22e90a853a9b
tobinbradley <tobin.bradley@gmail.com>
2013-11-23 13:39:59 -0500

    updated to leaflet 0.7, random tweaks

--- assets/scripts/page.js
+++ assets/scripts/page.js
@@ -79,8 +79,12 @@ $(document).ready(function () {
             maxZoom: 18
         }).setView([35.260, -80.827],10);
     // Mecklenburg Base Layer
-    //L.tileLayer("http://maps.co.mecklenburg.nc.us/tiles/meckbase/{y}/{x}/{z}.png").addTo(map);
+    //L.tileLayer("http://maps.co.mecklenburg.nc.us/tiles/meckbase-desaturated/{y}/{x}/{z}.png").addTo(map);
 
+    // var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    //     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    //     maxZoom: 16
+    // }).addTo(map);
 
 
     queue()
