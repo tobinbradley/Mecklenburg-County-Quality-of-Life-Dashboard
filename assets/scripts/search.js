abf4e04a82aa444f0135d083d798db6ff36f5be8
tobinbradley <tobin.bradley@gmail.com>
2014-04-15 17:52:37 -0400

    fix for color breaks bug

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -148,6 +148,8 @@ function initTypeahead(msg, data) {
     });
 
     // placeholder for $#!@#$! IE
-    $('input, textarea').placeholder();
-    $('input').focus().blur();
+    if ($.support.placeholder) {
+        $('input, textarea').placeholder();
+        $('input').focus().blur();
+    }
 }
