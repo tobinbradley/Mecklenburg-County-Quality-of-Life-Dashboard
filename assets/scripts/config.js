6c393bc8fc68dd108b5711430c83e977f12a2f55
Lyzi Diamond <lyzidiamond@gmail.com>
2014-06-25 16:26:17 -0700

    Update conifg and index.html

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -5,26 +5,26 @@
 // http://leaflet-extras.github.io/leaflet-providers/preview/
 // Ex: http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 // You want to change this - our base tiles only cover Mecklenburg County NC.
-var baseTilesURL = "http://mcmap.org:3000/meckbase/{z}/{x}/{y}.png";
+var baseTilesURL = "http://{s}.tiles.mapbox.com/v3/codeforamerica.ijj7831e/{z}/{x}/{y}.png";
 
 // The basic geographic setup for your map: the minimum zoom level,
 // maximum zoom level, and the starting zoom level, the map center point, and when
 // the base tiles should become visible.
 var mapGeography = {
         minZoom: 9,
-        maxZoom: 17,
+        maxZoom: 12,
         defaultZoom: 10,
-        center: [35.260, -80.827],
+        center: [38.042,-84.515],
         baseTileVisible: 15
     };
 
 // Neighborhoods name in your TopoJSON file. This is usually the name of the shapefile
 // or geojson file you converted from.
-var neighborhoods = "npa";
+var neighborhoods = "council";
 
 // If you have an additional data layer in your TopoJSON file, name it here.
 // Otherwise comment it out.
-var overlay = "istates";
+//var overlay = "istates";
 
 // Number of color breaks/quantiles in the map and bar chart.
 // Note the rule is 5 to 7 color breaks on a choropleth map. Don't be
