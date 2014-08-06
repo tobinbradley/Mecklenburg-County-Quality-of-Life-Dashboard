7dd1ad15e90e156ae54a61b3800ca92e5fc657d2
Lyzi Diamond <lyzidiamond@gmail.com>
2014-08-06 10:28:57 -0700

    Add conversion function for month numbers to month names

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,29 @@ jQuery.support.placeholder = (function(){
     return 'placeholder' in i;
 })();
 
+function nameMonth(year) {
+    var monthNumber = {
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December"
+    };
+
+    var yearNums = year.replace('y_', '');
+    var yearMonth = yearNums.split('-');
+    var sliderTitle = monthNumber[yearMonth[1]] + " " + yearMonth[0];
+    return sliderTitle;
+
+}
+
 // Slider change event
 function sliderChange(value) {
     $('.time-year').text(metricData[value].year.replace("y_", ""));
