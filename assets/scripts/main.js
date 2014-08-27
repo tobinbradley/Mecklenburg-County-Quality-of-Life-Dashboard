a1a4ac065d4e9dc69a25bec4d7f16fe5ac5f12af
Erik Schwartz <erik@erikschwartz.net>
2014-08-27 16:30:52 -0700

    Remove month over month logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -34,31 +34,6 @@ jQuery.support.placeholder = (function(){
     return 'placeholder' in i;
 })();
 
-// This function only relevant if using month-over-month
-
-// function nameMonth(year) {
-//     var monthNumber = {
-//         "01": "January",
-//         "02": "February",
-//         "03": "March",
-//         "04": "April",
-//         "05": "May",
-//         "06": "June",
-//         "07": "July",
-//         "08": "August",
-//         "09": "September",
-//         "10": "October",
-//         "11": "November",
-//         "12": "December"
-//     };
-
-//     var yearNums = year.replace('y_', '');
-//     var yearMonth = yearNums.split('-');
-//     var sliderTitle = monthNumber[yearMonth[1]] + " " + yearMonth[0];
-//     return sliderTitle;
-
-// }
-
 // Slider change event - month-over-month
 // function sliderChange(value) {
 
