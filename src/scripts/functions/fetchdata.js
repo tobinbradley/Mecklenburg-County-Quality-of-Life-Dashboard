6d8146315e1752ea6b93da9610c796817148e587
tobinbradley <tobin.bradley@gmail.com>
2015-06-17 13:40:16 -0400

    moved .get to .getJSON in fetchdata.js

--- src/scripts/functions/fetchdata.js
+++ src/scripts/functions/fetchdata.js
@@ -4,31 +4,31 @@
 // ****************************************
 function fetchAccuracy(m) {
     if (metricConfig[m].accuracy) {
-        return $.get("data/metric/" + m + "-accuracy.json");
+        return $.getJSON("data/metric/" + m + "-accuracy.json");
     }
     else { return [[]]; }
 }
 function fetchRaw(m) {
     if (metricConfig[m].raw_label || metricConfig[m].type === "weighted" || metricConfig[m].type === "sum") {
-        return $.get("data/metric/r" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/r" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchDenominator(m) {
     if (metricConfig[m].type === "weighted") {
-        return $.get("data/metric/d" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/d" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchNormalized(m) {
     if (metricConfig[m].type === "mean") {
-        return $.get("data/metric/n" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/n" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchGeometry() {
     if (d3Layer === undefined) {
-        return $.get("data/geography.topo.json");
+        return $.getJSON("data/geography.topo.json");
     }
     else { return [[]]; }
 }
--- src/tests/app.js
+++ src/tests/app.js
@@ -40510,31 +40510,31 @@ function drawBarChart() {
 // ****************************************
 function fetchAccuracy(m) {
     if (metricConfig[m].accuracy) {
-        return $.get("data/metric/" + m + "-accuracy.json");
+        return $.getJSON("data/metric/" + m + "-accuracy.json");
     }
     else { return [[]]; }
 }
 function fetchRaw(m) {
     if (metricConfig[m].raw_label || metricConfig[m].type === "weighted" || metricConfig[m].type === "sum") {
-        return $.get("data/metric/r" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/r" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchDenominator(m) {
     if (metricConfig[m].type === "weighted") {
-        return $.get("data/metric/d" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/d" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchNormalized(m) {
     if (metricConfig[m].type === "mean") {
-        return $.get("data/metric/n" + metricConfig[m].metric + ".json");
+        return $.getJSON("data/metric/n" + metricConfig[m].metric + ".json");
     }
     else { return [[]]; }
 }
 function fetchGeometry() {
     if (d3Layer === undefined) {
-        return $.get("data/geography.topo.json");
+        return $.getJSON("data/geography.topo.json");
     }
     else { return [[]]; }
 }
@@ -41240,7 +41240,7 @@ function updateSelected() {
     // bring selected geog to front if browser not crap
     if (!L.Browser.ie) {
         d3Layer.eachLayer(function (layer) {
-            if (layer._path.getAttribute("class").indexOf('d3-select') !== -1) {
+            if (layer._path && layer._path.getAttribute("class").indexOf('d3-select') !== -1) {
                 layer.bringToFront();
             }
         });
